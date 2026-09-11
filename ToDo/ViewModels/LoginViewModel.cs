e59234279a596cecfe540a01a82c0fd806169e2c
DilyaraD <dautova565@gmail.com>
2026-09-11 16:45:18 +0300

    Login

---
+++ ToDo/ViewModels/LoginViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using ToDo.Infastructure.Commands;
+using ToDo.ViewModels.Base;
+using ToDo.Views;
+
+namespace ToDo.ViewModels
+{
+    public class LoginViewModel : ViewModel
+    {
+
+        public LoginViewModel()
+        {
+            GoToRegisterCommand = new LambdaCommand(OnGoToRegisterCommandExecuted);
+            GoToCodeCommand = new LambdaCommand(OnGoToCodeCommandExecuted);
+        }
+        public ICommand GoToRegisterCommand { get; }
+        public ICommand GoToCodeCommand { get; }
+        private void OnGoToRegisterCommandExecuted(object p)
+        {
+            var registerWindow = new RegisterWindow();
+            registerWindow.ShowDialog();
+        }
+
+        private void OnGoToCodeCommandExecuted(object p)
+        {
+            var codeWindow = new CodeWindow();
+            codeWindow.ShowDialog();
+        }
+    }
+}
