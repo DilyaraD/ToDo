3a9f5ba66eb2de08fafda3d1838fc68216cb56ac
DilyaraD <dautova565@gmail.com>
2026-09-11 17:22:59 +0300

    RegisterWindow

---
+++ ToDo/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using ToDo.Models;
+
+namespace ToDo.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<Profile> Profiles { get; set; }
+        public DbSet<UserTask> UserTasks { get; set; }
+    }
+}
---
+++ ToDo/Models/Profile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.Models
+{
+    public class Profile
+    {
+        public int IdProfile { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public virtual UserTask UserTasks { get; set; }
+    }
+}
