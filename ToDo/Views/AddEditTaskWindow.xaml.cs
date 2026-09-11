1db216031cdeb8171e71ac301ac470d4e5afac56
DilyaraD <dautova565@gmail.com>
2026-09-11 15:35:19 +0300

    Views

--- ToDo/Views/AddEditTaskWindow.xaml.cs
+++ ToDo/Views/AddEditTaskWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ToDo
+namespace ToDo.Views
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для AddEditTaskWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AddEditTaskWindow : Window
     {
-        public MainWindow()
+        public AddEditTaskWindow()
         {
             InitializeComponent();
         }
