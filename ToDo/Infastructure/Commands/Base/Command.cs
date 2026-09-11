09b4cdd86121eecc62d38842af366d7bd6c0fa78
DilyaraD <dautova565@gmail.com>
2026-09-11 15:42:52 +0300

    Базовый класс команды

---
+++ ToDo/Infastructure/Commands/Base/Command.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ToDo.Infastructure.Commands.Base
+{
+    internal class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested += value;
+        }
+
+        public virtual bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
