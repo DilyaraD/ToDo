0b28217e4345ccb3a9cd38c5889a4b1babac4acc
DilyaraD <dautova565@gmail.com>
2026-09-19 19:30:45 +0300

    UnitTest 1 and 2

--- UnitTestToDo/UnitTest1.cs
+++ UnitTestToDo/UnitTest1.cs
@@ -21,9 +21,27 @@ namespace UnitTestToDo
         }
 
         [TestMethod]
-        public void TestMethod2_IsValidEmail()
+        public void TestMethod2_IsValidEmail_NoAtSymbol()
         {
-            Assert.IsTrue(_service.IsValidEmail("user.com"));
+            Assert.IsFalse(_service.IsValidEmail("user.com"));
+        }
+
+        [TestMethod]
+        public void TestMethod3_IsValidEmail_NoDomain() 
+        {
+            Assert.IsFalse(_service.IsValidEmail("user@"));
+        }
+
+        [TestMethod]
+        public void TestMethod4_IsValidEmail_Empty() 
+        {
+            Assert.IsFalse(_service.IsValidEmail(""));
+        }
+
+        [TestMethod]
+        public void TestMethod5_IsValidEmail_Whitespace() 
+        {
+            Assert.IsFalse(_service.IsValidEmail("    "));
         }
     }
 }
---
+++ UnitTestToDo/UnitTest2.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ToDo.Models;
+using ToDo.Services;
+
+namespace UnitTestToDo
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1_IsCompletedFalse()
+        {
+            var task = new UserTask();
+            Assert.IsFalse(task.IsCompleted);
+        }
+
+        [TestMethod]
+        public void TestMethod2_DueDateNull()
+        {
+            var task = new UserTask();
+            Assert.IsNull(task.DueDate);
+        }
+
+        [TestMethod]
+        public void TestMethod3_SetTitle()
+        {
+            var task = new UserTask { Title = "Test" };
+            Assert.AreEqual("Test", task.Title);
+        }
+
+        [TestMethod]
+        public void TestMethod4_SetDueDate()
+        {
+            var date = DateTime.Today.AddDays(1);
+            var task = new UserTask { DueDate = date };
+            Assert.AreEqual(date, task.DueDate);
+        }
+    }
+}
